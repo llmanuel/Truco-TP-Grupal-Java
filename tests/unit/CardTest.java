a871990c522160a5225a7852ba5e94a5d197a6c9
Alexis Daciuk <adaciuk@gmail.com>
2015-11-12 15:38:38 -0300

    Modifico test de las cartas para que reciba 3 parametros el constructor

--- tests/unit/CardTest.java
+++ tests/unit/CardTest.java
@@ -1,6 +1,8 @@
 package unit;
 
 import model.Card;
+import model.InvalidCardNumberException;
+import model.InvalidSuiteException;
 import org.junit.*;
 
 /**
@@ -8,18 +10,19 @@ import org.junit.*;
  */
 public class CardTest {
 
-    private static final Integer VALID_VALUE = 5;
+    private static final Integer VALID_NUMBER = 5;
     private static final String VALID_SUIT = "ORO";
+    private static final Integer VALID_VALUE = 12;
 
     @Test
-    public void canInstanciateCard(){
-       Card card = new Card(VALID_VALUE, VALID_SUIT);
+    public void canInstanciateCard() throws InvalidSuiteException, InvalidCardNumberException {
+       Card card = new Card(VALID_NUMBER, VALID_SUIT, VALID_VALUE);
     }
 
 
     @Test (expected = model.InvalidCardNumberException.class)
-    public void cantCreateCardeWithHigherValueThan12(){
-        Card card = new Card(14, VALID_SUIT);
+    public void cantCreateCardeWithHigherValueThan12() throws InvalidSuiteException, InvalidCardNumberException {
+        Card card = new Card(14, VALID_SUIT, VALID_VALUE);
 
     }
 
