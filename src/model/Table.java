6bdd94886d2806619c6a588eea476a26d2b460a1
llmanuco <llauromanuel@gmail.com>
2015-11-12 15:49:19 -0300

    subiendo los primeros test de Table e InvalidNumberOfPlayersException

--- src/model/Table.java
+++ src/model/Table.java
@@ -1,7 +1,29 @@
 package model;
 
+import java.util.LinkedList;
+
 /**
  * Created by alexdebian on 11/10/15.
  */
 public class Table {
+
+    private int slots;
+    private LinkedList<Player> PlayersInGame;
+
+    public Table(int slots) {
+
+        this.slots = slots;
+    }
+
+    public void letSitThese(LinkedList<Player> Players) throws InvalidNumberOfPlayersException{
+
+        if( (Players.size() == 2) || (Players.size() == 4) ||(Players.size() == 6))
+        this.PlayersInGame =Players;
+
+        else new InvalidNumberOfPlayersException();
+    }
+
+    public int getNumberOfPlayers() {
+        return (PlayersInGame.size());
+    }
 }
