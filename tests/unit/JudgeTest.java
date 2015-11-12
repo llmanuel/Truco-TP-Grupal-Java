6f4f36558e22feef9a33a858deea7bc089275ef8
Matias Rozanec <rozanecm@gmail.com>
2015-11-12 15:50:14 -0300

    Ajuste lo hecho a los cambios realizaos por Alex.

--- tests/unit/JudgeTest.java
+++ tests/unit/JudgeTest.java
@@ -1,6 +1,7 @@
 package unit;
 
 import model.Card;
+import model.InvalidCardNumberException;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -12,14 +13,25 @@ import model.Judge;
  */
 public class JudgeTest {
 
-    Card card1 = new Card(7, "ESPADA");
-    Card card2 = new Card(2, "BASTO");
-    Card card3 = new Card(1, "COPA");
-    Card card4 = new Card(12, "ORO");
-    Card card5 = new Card(7, "BASTO");
-    Card card6 = new Card(6, "BASTO");
-    Card card7 = new Card(6, "ORO");
+        Card card1;
+        Card card2;
+        Card card3;
+        Card card4;
+        Card card5;
+        Card card6;
+        Card card7;
 
+
+    @Before
+    public void setUp() throws Exception {
+        card1 = new Card(7, "ESPADA", 7);
+        card2 = new Card(2, "BASTO", 6);
+        card3 = new Card(1, "COPA", 5);
+        card4 = new Card(12, "ORO", 4);
+        card5 = new Card(7, "BASTO", 3);
+        card6 = new Card(6, "BASTO", 2);
+        card7 = new Card(6, "ORO", 1);
+    }
 
     @Test
     public void canInstantiateAJudge(){
