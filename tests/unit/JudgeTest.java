58cb98d307d8edfc57cbd2d50f726a3d2942d99a
Matias Rozanec <rozanecm@gmail.com>
2015-11-12 13:24:47 -0300

    Agrego lo basico de carta (atributos de palo y valor; string e Integer respectivamente). Arranque con prueba de comparacion de cartas, pero deje en el medio por consulta.

--- tests/unit/JudgeTest.java
+++ tests/unit/JudgeTest.java
@@ -1,5 +1,6 @@
 package unit;
 
+import model.Card;
 import org.junit.Test;
 import model.Judge;
 
@@ -11,6 +12,15 @@ public class JudgeTest {
     @Test
     public void canInstantiateAJudge(){
         Judge judge = new Judge();
+    }
+
+    @Test
+    public void correctlyComparesCardsValue(){
+        Judge judge = new Judge();
+        Card card1 = new Card(7, "unPalo");
+        Card card2 = new Card(9, "unPalo");
+
+//        judge.
     }
 
 }
