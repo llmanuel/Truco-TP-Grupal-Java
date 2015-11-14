0408e0d7d18f3b6e83a5560f935f7af7beaf0f61
Matias Rozanec <rozanecm@gmail.com>
2015-11-13 22:50:42 -0300

    Creo test de creacion de player.

---
+++ tests/unit/HumanPlayerTest.java
@@ -0,0 +1,13 @@
+package unit;
+
+import model.HumanPlayer;
+import model.Player;
+import org.junit.Test;
+
+public class HumanPlayerTest {
+    @Test
+    public void canInstatiateHumanPlayer(){
+        Player humanPlayer = new HumanPlayer();
+    }
+
+}
--- tests/unit/PlayerTest.java
+++
@@ -1,7 +0,0 @@
-package unit;
-
-/**
- * Created by Snemanje on 12/11/15.
- */
-public class PlayerTest {
-}
