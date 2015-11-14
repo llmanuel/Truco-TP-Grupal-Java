f4d3ab9ab76fa2046ec0a423999f88861995c959
Lucas Risaro <lucasrisaro@gmail.com>
2015-11-14 03:59:29 -0300

    correccion de comentarios

--- tests/unit/CroupierTest.java
+++ tests/unit/CroupierTest.java
@@ -14,18 +14,15 @@ public class CroupierTest {
 
     private Croupier croupier;
 
-<<<<<<< HEAD
     @Before
     public void setup() {
     	
          croupier = new Croupier();
     }
 
-=======
->>>>>>> f93a68c1f68c76be50681e6923c69ddaacbd7528
+
     @Test
     public void testCroupierCanGiveAHand() throws InvalidSuiteException, InvalidCardNumberException {
-        //Croupier croupier = new Croupier();
 
         Assert.assertThat( croupier.giveHand(), instanceOf( Hand.class ) );
 
