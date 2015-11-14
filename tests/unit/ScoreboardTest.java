1e894bf61e1eb086d67ba251bab3919fcaf7ef71
Lucas Risaro <lucasrisaro@gmail.com>
2015-11-14 00:51:55 -0300

    'Se incorporo el incrementador de puntuacion a scoreboard y un metodo para obtener los puntajes de los jugadores'

--- tests/unit/ScoreboardTest.java
+++ tests/unit/ScoreboardTest.java
@@ -1,5 +1,6 @@
 package unit;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import model.Scoreboard;
@@ -10,11 +11,33 @@ import model.Scoreboard;
 public class ScoreboardTest {
 	
 	private static final Integer numberOfPlayers = 2;
+	private static final Integer playerID = 2;
 	
     @Test
     public void canInstantiateScoreboard(){
         Scoreboard scoreboard = new Scoreboard(numberOfPlayers);
     }
-
-
+    
+    @Test
+    public void scoreboardSuccessfulyInitialize(){
+    	
+    	Scoreboard scoreboard = new Scoreboard(numberOfPlayers);
+    	
+    	Assert.assertTrue( 0 == scoreboard.getScoreOfPlayer(playerID));
+    	
+    }
+    
+    @Test
+    public void canIncreaseTheScoreOfThePlayer(){
+    	
+    	Scoreboard scoreboard = new Scoreboard(numberOfPlayers);
+    	
+    	int score = 2;
+    	int oldScore = scoreboard.getScoreOfPlayer(playerID);
+    	
+    	scoreboard.increseTheScoreOf(playerID , score);
+    	
+    	Assert.assertTrue(oldScore + score == scoreboard.getScoreOfPlayer(playerID));
+    	
+    }
 }
