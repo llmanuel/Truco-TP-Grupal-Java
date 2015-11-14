be676f76f594732a8b36e0f809dce0150db97f6f
Lucas Risaro <lucasrisaro@gmail.com>
2015-11-13 23:29:45 -0300

    'Se agrego el metodo calculateEnvido()'

--- src/model/CpuPlayer.java
+++ src/model/CpuPlayer.java
@@ -4,4 +4,10 @@ package model;
  * Created by alexdebian on 11/10/15.
  */
 public class CpuPlayer implements Player {
+
+	@Override
+	public int calculateEnvido(Card... cards) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 }
