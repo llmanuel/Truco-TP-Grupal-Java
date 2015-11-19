9885248e55c53febaefeba449d0833e9b7a209e0
Alexis Daciuk <adaciuk@gmail.com>
2015-11-18 22:22:07 -0300

    Creo que nada

--- tests/integration/PlayerAndHandTest.java
+++ tests/integration/PlayerAndHandTest.java
@@ -1,14 +1,10 @@
 package integration;
 
-import java.util.LinkedList;
-
 import model.Croupier;
-import org.junit.Assert;
-import org.junit.Test;
-
-import model.Card;
 import model.Hand;
 import model.HumanPlayer;
+import org.junit.Assert;
+import org.junit.Test;
 
 /**
  * Created by alexdebian on 11/10/15.
@@ -17,7 +13,6 @@ public class PlayerAndHandTest {
 
 	@Test
 	public void PlayerSavesCorrectHand() throws Exception {
-
         Croupier croupier = new Croupier();
         HumanPlayer humanPlayer = new HumanPlayer( 1 );
 
