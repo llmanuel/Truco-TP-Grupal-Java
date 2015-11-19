a573137289569bf7675e0164d93adcbd4ad62b95
Alexis Daciuk <adaciuk@gmail.com>
2015-11-19 14:19:19 -0300

    Agrego atributo para guardar los miembros y metodo para saber si un miembro pertenece al Team

--- src/model/Team.java
+++ src/model/Team.java
@@ -1,5 +1,15 @@
 package model;
 
-public class Team {
+import java.util.LinkedList;
 
+public class Team {
+    LinkedList<Player> members;
+
+    public Team(LinkedList<Player> newMembers) {
+        this.members = newMembers;
+    }
+
+    public boolean isMember(Player player1) {
+        return this.members.contains( player1 );
+    }
 }
