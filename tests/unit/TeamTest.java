02df6f49fede7f50f4d73ec5abb40d79bd380606
Alexis Daciuk <adaciuk@gmail.com>
2015-11-19 14:25:04 -0300

    Agrego test para la clase Team

--- tests/unit/TeamTest.java
+++ tests/unit/TeamTest.java
@@ -2,16 +2,47 @@ package unit;
 
 import static org.junit.Assert.*;
 
+import model.HumanPlayer;
+import model.Player;
+import org.junit.Assert;
 import org.junit.Test;
 
 import model.Team;
+
+import java.util.LinkedList;
 
 public class TeamTest {
 
 	@Test
 	public void canCreateATeam() {
-		
-		Team team = new Team();
+        LinkedList<Player> members = new LinkedList<Player>();
+
+        Player player1, player2;
+        player1 = new HumanPlayer( 1 );
+        player2 = new HumanPlayer( 2 );
+
+        members.add( player1 );
+        members.add( player2 );
+
+		Team team = new Team( members );
 	}
+
+	@Test
+    public void teamKnowsItsPlayers(){
+
+
+        LinkedList<Player> newMembers = new LinkedList<Player>();
+
+        Player player1, player2;
+        player1 = new HumanPlayer( 1 );
+        player2 = new HumanPlayer( 2 );
+
+        newMembers.add( player1 );
+        newMembers.add( player2 );
+
+        Team team = new Team( newMembers );
+
+        Assert.assertTrue( team.isMember(player1) );
+    }
 
 }
