de207004191de6026a53a8b804a8c5cce24cb2b2
Alexis Daciuk <adaciuk@gmail.com>
2015-11-19 13:39:51 -0300

    Agrego test para asegurarme que el envido se calcula bien

--- tests/integration/PlayerAndHandTest.java
+++ tests/integration/PlayerAndHandTest.java
@@ -1,11 +1,11 @@
 package integration;
 
-import model.Croupier;
-import model.Hand;
-import model.HumanPlayer;
+import model.*;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.LinkedList;
 
 public class PlayerAndHandTest {
 
@@ -20,12 +20,32 @@ public class PlayerAndHandTest {
     }
 
 	@Test
-	public void PlayerSavesCorrectHand() throws Exception {
+	public void playerSavesCorrectHand() throws Exception {
         Hand hand =  croupier.giveHand();
 
         humanPlayer.setHand(hand);
         Assert.assertTrue(humanPlayer.getHand() == hand);
 	}
+
+    @Test
+    public void playerReturnsEnvidoCorrectly() throws InvalidSuiteException, InvalidCardNumberException {
+        Card card1 = new Card(5, "BASTO", 1);
+        Card card2 = new Card(7, "BASTO", 5);
+        Card card3 = new Card(7, "ESPADA", 6);
+
+        LinkedList<Card> cards = new LinkedList<Card>();
+        cards.add(card1);
+        cards.add(card2);
+        cards.add(card3);
+
+        Hand newHand = new Hand( cards );
+
+        this.humanPlayer.setHand( newHand );
+
+        Assert.assertTrue( this.humanPlayer.calculateEnvido() == 32 );
+
+
+    }
 
 
 
