3825017979dd386514464696b32f5723a55864cd
Alexis Daciuk <adaciuk@gmail.com>
2015-11-19 15:49:34 -0300

    Agrego clase Score para manejar los puntajes

--- src/model/Score.java
+++ src/model/Score.java
@@ -1,12 +1,25 @@
 package model;
 
+import model.CanHaveScore;
 
 public class Score {
-    private final CanHaveScore canHaveScore;
+    private CanHaveScore canHaveScore;
     private int score;
 
     public Score(CanHaveScore canHaveScore) {
         this.canHaveScore = canHaveScore;
         this.score = 0;
+    }
+
+    public int getScore() {
+        return this.score;
+    }
+
+    public CanHaveScore getMember() {
+        return this.canHaveScore;
+    }
+
+    public void increaseScoreBy(int scoreIncrease) {
+        this.score =+ scoreIncrease;
     }
 }
