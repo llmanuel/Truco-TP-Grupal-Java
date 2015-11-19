aee6ceb00a1f5b9873ff4b277cda8fe7507e7269
Alexis Daciuk <adaciuk@gmail.com>
2015-11-19 10:43:46 -0300

    Agrego tests de integracion entre Table y Test

--- tests/integration/TableAndJudge.java
+++ tests/integration/TableAndJudge.java
@@ -1,3 +1,41 @@
+import model.*;
+import org.junit.Before;
+
+import java.util.LinkedList;
 
 public class TableAndJudge {
+
+
+    private Table table;
+    private Judge judge;
+    private Scoreboard scoreboard;
+    private LinkedList<Player> FourPlayers;
+
+    @Before
+    public void setup() throws InvalidNumberOfPlayersException {
+        HumanPlayer myPLayer = new HumanPlayer(1);
+        HumanPlayer otherPLayer = new HumanPlayer(2);
+        HumanPlayer charles = new HumanPlayer(3);
+        HumanPlayer nick = new HumanPlayer(4);
+
+        FourPlayers = new LinkedList<Player>();
+        FourPlayers.add(myPLayer);
+        FourPlayers.add(otherPLayer);
+        FourPlayers.add(charles);
+        FourPlayers.add(nick);
+
+
+        this.scoreboard = new Scoreboard( 4 )
+
+        this.table = new Table();
+        this.table.letSitThese( FourPlayers );
+        this.table.setGame();
+
+        this.judge = new Judge( this.scoreboard );
+
+    }
+
+    
+
+
 }
