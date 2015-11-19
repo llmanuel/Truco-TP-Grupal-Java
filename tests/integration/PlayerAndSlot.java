aa8f52a9f363b46a75f337a151960524e0c32da3
Alexis Daciuk <adaciuk@gmail.com>
2015-11-19 13:21:15 -0300

    Agrego test

---
+++ tests/integration/PlayerAndSlot.java
@@ -0,0 +1,34 @@
+import model.*;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class PlayerAndSlot {
+    Player player;
+    Slot slot;
+    Croupier croupier;
+
+
+    @Before
+    public void setup() throws InvalidSuiteException, InvalidCardNumberException {
+        this.croupier = new Croupier();
+
+        this.player = new HumanPlayer( 1 );
+
+        this.slot = new Slot( this.player );
+        this.player.setSlot( this.slot );
+
+        this.player.setHand( this.croupier.giveHand() );
+    }
+
+    @Test
+    public void playerCanPlayACertainCard() throws NotCardThrownException {
+        Card cardToPlay = this.player.getHand().getCards().getFirst();
+
+        this.player.playCard( cardToPlay );
+
+        Assert.assertTrue( this.slot.getLastOne() == cardToPlay);
+    }
+
+
+}
