99037e176727514cfc061bdae694cb1115b1702d
Alexis Daciuk <adaciuk@gmail.com>
2015-11-19 11:11:09 -0300

    Cambio el nombre de la clase de Slots a Slot, ya que representa un Slot solamente

---
+++ src/model/Slot.java
@@ -0,0 +1,60 @@
+package model;
+
+
+import java.util.LinkedList;
+
+/**
+ * Created by manuel on 13/11/15.
+ */
+public class Slot {
+    private Player player;
+    private LinkedList<Card> cards;
+
+    public Slot(Player player) {
+        this.player = player;
+        cards = new LinkedList<Card>();
+    }
+
+    public boolean receiveCard(Card newCard) {
+        this.cards.addLast(newCard);
+        return true;
+    }
+
+    public boolean isYourPlayer(Player myPlayer) {
+        return (this.player.getIdNumber() == myPlayer.getIdNumber());
+    }
+
+    public Card getFirstOne() throws NotCardThrownException{
+
+        if(!cards.isEmpty())
+            return (cards.getFirst());
+        else throw new NotCardThrownException();
+    }
+
+    public Card getSecondOne() throws NotCardThrownException{
+        if(cards.size() == 2)
+           return (cards.get(1));
+        else throw new NotCardThrownException();
+    }
+
+    public Card getThirdOne() throws NotCardThrownException{
+        if(cards.size() == 3)
+            return (cards.getLast());
+        else throw new NotCardThrownException();
+    }
+
+    public Card getLastOne() throws NotCardThrownException{
+        if(!cards.isEmpty())
+            return (cards.getLast());
+        else throw new NotCardThrownException();
+    }
+
+    public void throwCards() {
+
+        cards = new LinkedList<Card>();
+    }
+
+    public Player getPlayer() {
+        return this.player;
+    }
+}
---
+++ tests/unit/SlotTest.java
@@ -0,0 +1,77 @@
+package unit;
+
+import model.*;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by manuel on 13/11/15.
+ */
+public class SlotTest {
+
+    Slot slot;
+    HumanPlayer myPlayer;
+    Card first;
+    Card second;
+    Card thirdCard;
+
+    @Before
+    public void setup() throws InvalidSuiteException, InvalidCardNumberException {
+        myPlayer = new HumanPlayer(1);
+        slot = new Slot(myPlayer);
+        first = new Card(1,"ESPADA", 14);
+        second = new Card(1,"ORO", 8);
+        thirdCard = new Card(1,"BASTO", 13);
+    }
+
+    @Test
+    public void slotsCanReceiveCards()  {
+
+        Assert.assertTrue(slot.receiveCard(first));
+        Assert.assertTrue(slot.receiveCard(second));
+        Assert.assertTrue(slot.receiveCard(thirdCard));
+    }
+
+    @Test
+    public void slotsKnowsItsOwner() throws InvalidSuiteException, InvalidCardNumberException {
+
+        Assert.assertTrue(slot.isYourPlayer(myPlayer));
+    }
+
+    @Test
+    public void slotsReturnsFalseIfIsNotHIsPlayer() throws InvalidSuiteException, InvalidCardNumberException {
+
+        HumanPlayer otherPlayer = new HumanPlayer(2);
+        Assert.assertFalse(slot.isYourPlayer(otherPlayer));
+    }
+
+    @Test
+    public void slotsReturnsTheFirstCard() throws NotCardThrownException{
+        slot.receiveCard(first);
+        Assert.assertEquals(first.getNumber(),slot.getFirstOne().getNumber());
+    }
+
+    @Test
+    public void slotsReturnsTheSecondCard() throws NotCardThrownException{
+        slot.receiveCard(first);
+        slot.receiveCard(second);
+        Assert.assertEquals(second.getNumber(),slot.getSecondOne().getNumber());
+    }
+
+    @Test
+    public void slotsReturnsTheThirdCard() throws NotCardThrownException{
+        slot.receiveCard(first);
+        slot.receiveCard(second);
+        slot.receiveCard(thirdCard);
+        Assert.assertEquals(thirdCard.getNumber(),slot.getThirdOne().getNumber());
+    }
+
+    @Test (expected = NotCardThrownException.class)
+    public void slotsThrowsAwayTheCardsWhenItIsTellToDoSo() throws NotCardThrownException {
+
+       slot.receiveCard(first);
+       slot.throwCards();
+       Assert.assertEquals(first.getNumber(),slot.getFirstOne().getNumber());
+    }
+}
