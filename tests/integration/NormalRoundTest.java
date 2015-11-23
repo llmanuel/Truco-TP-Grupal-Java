4d6e8b2018972a817291b80f4573c079d7c2df5d
Alexis Daciuk <adaciuk@gmail.com>
2015-11-23 16:41:53 -0300

    Agrego mas cosas al setup

--- tests/integration/NormalRoundTest.java
+++ tests/integration/NormalRoundTest.java
@@ -2,6 +2,8 @@
 import model.Card;
 import model.Exceptions.InvalidCardNumberException;
 import model.Exceptions.InvalidSuiteException;
+import model.Hand;
+import model.HumanPlayer;
 import org.junit.Before;
 
 import java.util.LinkedList;
@@ -9,7 +11,12 @@ import java.util.LinkedList;
 public class NormalRoundTest {
 
     private Card card1,card2,card3,card4,card5,card6;
-    private LinkedList<Card> hand1;
+    private LinkedList<Card> hand1Cards;
+    private LinkedList<Card> hand2Cards;
+    private Hand hand1;
+    private Hand hand2;
+    private HumanPlayer player1;
+    private HumanPlayer player2;
 
 
     @Before
@@ -22,9 +29,24 @@ public class NormalRoundTest {
         card5 = new Card(3, "ORO", 5);
         card6 = new Card(7, "ORO", 5);
         
-        hand1 = new LinkedList<Card>();
+        hand1Cards = new LinkedList<Card>();
+        hand1Cards.add( card1 );
+        hand1Cards.add( card2 );
+        hand1Cards.add( card3 );
 
+        hand2Cards = new LinkedList<Card>();
+        hand2Cards.add( card4 );
+        hand2Cards.add( card5 );
+        hand2Cards.add( card6 );
 
+        hand1 = new Hand( hand1Cards );
+        hand2 = new Hand( hand2Cards );
+
+        player1 = new HumanPlayer( 1 );
+        player2 = new HumanPlayer( 2 );
+
+        player1.setHand( hand1 );
+        player2.setHand( hand2 );
 
 
 
