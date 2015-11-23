0e53829f15da8d1e371f40a1115c0851c07abe64
llmanuco <llauromanuel@gmail.com>
2015-11-22 23:06:26 -0300

    agregado de instancia de Table en los Games

--- src/model/ReTruco.java
+++ src/model/ReTruco.java
@@ -2,6 +2,11 @@ package model;
 
 public class ReTruco implements Games {
     int points = 3;
+    private Table theTable;
+
+    public void ReTruco(Table theTable){
+        this.theTable = theTable;
+    }
 
     @Override
     public int getPoints(){
--- src/model/RealEnvido.java
+++ src/model/RealEnvido.java
@@ -2,6 +2,11 @@ package model;
 
 public class RealEnvido implements Games {
     int points = 3;
+    private Table theTable;
+
+    public void RealEnvido(Table theTable){
+        this.theTable = theTable;
+    }
 
     @Override
     public int getPoints(){
