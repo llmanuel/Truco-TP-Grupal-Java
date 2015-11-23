0a36996e90d4e9d10b7147b5109cb2bc82647877
Alexis Daciuk <adaciuk@gmail.com>
2015-11-23 17:36:23 -0300

    Voy agregando tests

--- tests/unit/ScoreboardTest.java
+++ tests/unit/ScoreboardTest.java
@@ -1,9 +1,9 @@
-package unit;
-
 import model.CanHaveScore;
+import model.Exceptions.CanHaveScoreDoesntExistException;
 import model.HumanPlayer;
 import model.Scoreboard;
 import model.Table;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -31,6 +31,29 @@ public class ScoreboardTest {
     @Test
     public void canInstantiateScoreboard(){
         Scoreboard scoreboard = new Scoreboard( playerList, table );
+
+    }
+
+    @Test
+    public void scoreboardStartsWithZeroPoints() throws CanHaveScoreDoesntExistException {
+        Scoreboard scoreboard = new Scoreboard( playerList, table );
+
+        LinkedList<CanHaveScore> players = scoreboard.getPlayers();
+
+        Assert.assertEquals( 0, scoreboard.getPointsOf( players.getFirst()) , 0.001 );
+        Assert.assertEquals( 0, scoreboard.getPointsOf( players.getLast()) , 0.001 );
+
+    }
+
+    @Test
+    public void pointsIncreaseCorrectly() throws CanHaveScoreDoesntExistException {
+        Scoreboard scoreboard = new Scoreboard( playerList, table );
+
+        LinkedList<CanHaveScore> players = scoreboard.getPlayers();
+
+        scoreboard.increaseTheScoreOf( players.getFirst(), 5 );
+
+        Assert.assertEquals( 5, scoreboard.getPointsOf( players.getFirst()) , 0.001 );
 
     }
 
