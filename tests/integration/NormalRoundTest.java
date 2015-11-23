9c24cea5e7a628b411bc45e192356bd9cf888832
Alexis Daciuk <adaciuk@gmail.com>
2015-11-23 01:56:40 -0300

    Agrego el progreso hasta ahora

--- tests/integration/NormalRoundTest.java
+++ tests/integration/NormalRoundTest.java
@@ -1,2 +1,36 @@
+
+import model.Card;
+import model.Exceptions.InvalidCardNumberException;
+import model.Exceptions.InvalidSuiteException;
+import org.junit.Before;
+
+import java.util.LinkedList;
+
 public class NormalRoundTest {
+
+    private Card card1,card2,card3,card4,card5,card6;
+    private LinkedList<Card> hand1;
+
+
+    @Before
+    public void setup() throws InvalidSuiteException, InvalidCardNumberException {
+        card1 = new Card(5, "BASTO", 1);
+        card2 = new Card(7, "BASTO", 5);
+        card3 = new Card(7, "ESPADA", 6);
+
+        card4 = new Card(12, "ESPADA", 6);
+        card5 = new Card(3, "ORO", 5);
+        card6 = new Card(7, "ORO", 5);
+        
+        hand1 = new LinkedList<Card>();
+
+
+
+
+
+    }
+
+
+
+
 }
