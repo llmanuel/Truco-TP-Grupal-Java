1d719c82b6e003bc378a3e19305ef871c5a04acb
Alexis Daciuk <adaciuk@gmail.com>
2015-11-26 11:23:36 -0300

    Elimino todo

--- src/model/Builder.java
+++ src/model/Builder.java
@@ -1,27 +1,10 @@
 package model;
 
-import java.util.LinkedList;
+
 
 public class Builder {
 
     public Builder(int numberOfPlayers){
 
-        LinkedList<Player> thePlayers = new LinkedList<Player>();
-        LinkedList<CanHaveScore> theCanHaveScorePL = new LinkedList<CanHaveScore>();
-
-        /*creating required n. of players*/
-        for (int i = 1; i <= numberOfPlayers; i++){
-            HumanPlayer newPlayer = new HumanPlayer(i);
-            thePlayers.add(newPlayer);
-            theCanHaveScorePL.add(newPlayer);
-        }
-
-        Table table = new Table();
-        Scoreboard theScoreboard = new Scoreboard( theCanHaveScorePL, table );
-
-        Judge judge = new Judge(theScoreboard);
-
-        table.getAJudge(judge);
-        table.getScoreBoard(theScoreboard);
     }
 }
