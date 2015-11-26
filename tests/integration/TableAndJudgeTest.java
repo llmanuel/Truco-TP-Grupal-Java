47c20c15a83a97c60755f9253bffb53a4afc40bb
Alexis Daciuk <adaciuk@gmail.com>
2015-11-26 11:23:58 -0300

    Arreglo la instanciacion de scoreboard

--- tests/integration/TableAndJudgeTest.java
+++ tests/integration/TableAndJudgeTest.java
@@ -15,31 +15,53 @@ public class TableAndJudgeTest {
     private Judge judge;
     private Scoreboard scoreboard;
     private LinkedList<Player> fourPlayers;
-    private LinkedList<CanHaveScore> fourPlayersScoreboard;
+    private LinkedList<Team> fourPlayersTeams;
+    private Team teamMyPlayer;
+    private Team teamOtherPlayer;
+    private Team teamCharles;
+    private Team teamNick;
 
     @Before
     public void setup() throws InvalidNumberOfPlayersException {
-        HumanPlayer myPLayer = new HumanPlayer(1);
+        HumanPlayer myPlayer = new HumanPlayer(1);
+        LinkedList<Player> myPlayerList= new LinkedList<Player>(  );
+        myPlayerList.add( myPlayer );
+
         HumanPlayer otherPLayer = new HumanPlayer(2);
+        LinkedList<Player> otherPlayerList = new LinkedList<Player>(  );
+        myPlayerList.add( otherPLayer );
+
         HumanPlayer charles = new HumanPlayer(3);
+        LinkedList<Player> charlesList= new LinkedList<Player>(  );
+        myPlayerList.add( charles );
+
         HumanPlayer nick = new HumanPlayer(4);
+        LinkedList<Player> nickList = new LinkedList<Player>(  );
+        myPlayerList.add( nick );
+
+
+        teamMyPlayer = new Team( myPlayerList );
+        teamOtherPlayer = new Team( otherPlayerList );
+        teamCharles = new Team( charlesList );
+        teamNick = new Team( nickList );
+
 
         fourPlayers = new LinkedList<Player>();
-        fourPlayers.addLast(myPLayer);
+        fourPlayers.addLast(myPlayer);
         fourPlayers.addLast(otherPLayer);
         fourPlayers.addLast(charles);
         fourPlayers.addLast(nick);
 
-        fourPlayersScoreboard = new LinkedList<CanHaveScore>();
-        fourPlayersScoreboard.addLast(myPLayer);
-        fourPlayersScoreboard.addLast(otherPLayer);
-        fourPlayersScoreboard.addLast(charles);
-        fourPlayersScoreboard.addLast(nick);
+        fourPlayersTeams = new LinkedList<Team>();
+        fourPlayersTeams.addLast(teamMyPlayer);
+        fourPlayersTeams.addLast(teamOtherPlayer);
+        fourPlayersTeams.addLast(teamCharles);
+        fourPlayersTeams.addLast(teamNick);
 
         this.table = new Table();
 
 
-        this.scoreboard = new Scoreboard( fourPlayersScoreboard, this.table );
+        this.scoreboard = new Scoreboard( fourPlayersTeams, this.table );
 
         this.table.letSitThese( fourPlayers );
         this.table.setGame();
@@ -54,46 +76,7 @@ public class TableAndJudgeTest {
         this.judge.setWinnerOfTheRound( this.table.getSlots(), actualGame);
     }
 
-    @Test
-    public void tableCanPassSlotsAndGameToJudgeCorrectly() throws InvalidSuiteException, InvalidCardNumberException, CanHaveScoreDoesntExistException, NotYourTurnException {
-        Games actualGame = new Truco(this.table, 0);
-        Card card1 = new Card(1, "BASTO", 1);
-        Card card2 = new Card(3, "ORO", 3);
-        Card card3 = new Card(4, "ORO", 4);
-        Card card4 = new Card(5, "ORO", 5);
 
-        LinkedList<Card> listOfCards = new LinkedList<Card>();
-        listOfCards.add(card1);
-        listOfCards.add(card2);
-        listOfCards.add(card3);
-        listOfCards.add(card4);
-
-        Hand hand = new Hand(listOfCards);
-
-        fourPlayers.get(0).sitOnTable(table);
-        fourPlayers.get(1).sitOnTable(table);
-        fourPlayers.get(2).sitOnTable(table);
-        fourPlayers.get(3).sitOnTable(table);
-
-        fourPlayers.get(0).setHand(hand);
-        fourPlayers.get(1).setHand(hand);
-        fourPlayers.get(2).setHand(hand);
-        fourPlayers.get(3).setHand(hand);
-
-        fourPlayers.get(0).playCard(card1);
-        fourPlayers.get(1).playCard(card2);
-        fourPlayers.get(2).playCard(card3);
-        fourPlayers.get(3).playCard(card4);
-
-        boolean exceptionRised = false;
-        try {
-            this.judge.setWinnerOfTheRound( this.table.getSlots(), actualGame);
-        } catch (NotCardThrownException e) {
-            exceptionRised = true;
-        }
-
-        Assert.assertFalse(exceptionRised);
-    }
 
 
 }
