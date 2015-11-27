5fcea5a1ad145ecdf550f9a2e12e11e8be92ac03
Alexis Daciuk <adaciuk@gmail.com>
2015-11-27 14:48:49 -0300

    Agrego test para el CpuPlayer

---
+++ tests/unit/CpuPlayerTest.java
@@ -0,0 +1,10 @@
+import model.CpuPlayer;
+import org.junit.Test;
+
+public class CpuPlayerTest {
+
+    @Test
+    public void canInstanciateACpuPlayer(){
+        CpuPlayer cpuplayer = new CpuPlayer( 1 );
+    }
+}
