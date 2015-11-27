461c27a72feae85c70b2d428b2ccb5200f330f1c
Matias Rozanec <rozanecm@gmail.com>
2015-11-27 12:41:03 -0300

    Creo metodo de creacion de jugadores

--- src/model/Builder.java
+++ src/model/Builder.java
@@ -3,10 +3,26 @@ package model;
 
 import model.Exceptions.InvalidNumberOfPlayersException;
 
+import java.util.LinkedList;
+
 public class Builder {
+
+    LinkedList<Player> players;
 
     public Builder(int numberOfPlayers) throws InvalidNumberOfPlayersException {
         validateNumberOfPlayers(numberOfPlayers);
+        createPlayers(numberOfPlayers);
+    }
+
+    private void createPlayers(int numberOfPlayers) {
+        switch (numberOfPlayers){
+//            case 2: createTwoPlayers();
+//                break;
+//            case 4: createFourPlayers();
+//                break;
+//            case 6: createSixPlayers();
+//                break;
+        }
     }
 
     private void validateNumberOfPlayers(int numberOfPlayers) throws InvalidNumberOfPlayersException {
