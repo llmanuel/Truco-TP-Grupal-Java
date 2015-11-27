474a0e2bfd5cde8ae111f71f0054a2274f6066aa
Alexis Daciuk <adaciuk@gmail.com>
2015-11-27 15:13:08 -0300

    Arreglo estilo de codigo

--- src/model/Judge.java
+++ src/model/Judge.java
@@ -1,18 +1,18 @@
 package model;
 
-import model.Exceptions.TeamDoesntExistException;
 import model.Exceptions.NotCardThrownException;
+import model.Exceptions.TeamDoesntExistException;
 import model.TableStates.Games;
 
 import java.util.LinkedList;
 
 public class Judge {
-	
-	private LinkedList<Player> players = new LinkedList<Player>();
+
+    private LinkedList<Player> players = new LinkedList<Player>();
     private Scoreboard scoreboard;
     private long[] roundWinsPerTeam;
 
-    public Judge(Scoreboard scoreboard){
+    public Judge(Scoreboard scoreboard) {
         this.scoreboard = scoreboard;
         roundWinsPerTeam = new long[]{0, 0};
     }
@@ -27,49 +27,45 @@ public class Judge {
     }
 
     public boolean isCardAHigherThanCardB(Card cardA, Card cardB) {
-        return (cardA.getValue())>(cardB.getValue());
+        return (cardA.getValue()) > (cardB.getValue());
     }
 
-	public void setWinnerOfEnvido(Games actualGame) throws TeamDoesntExistException {
-		int maximunValueOfEnvido = 0;
+    public void setWinnerOfEnvido(Games actualGame) throws TeamDoesntExistException {
+        int maximunValueOfEnvido = 0;
         Player envidoWinner = null;
 
-		for (Player actualPlayer : this.players) {
-			if (actualPlayer.calculateEnvido() > maximunValueOfEnvido ) {
+        for (Player actualPlayer : this.players) {
+            if (actualPlayer.calculateEnvido() > maximunValueOfEnvido) {
                 envidoWinner = actualPlayer;
                 maximunValueOfEnvido = actualPlayer.calculateEnvido();
             }
-		}
+        }
         this.scoreboard.increaseTheScoreOf( envidoWinner, actualGame );
-
-	}
-	
-	public Player setWinnerOfTheRound(LinkedList<Slot> slots) throws NotCardThrownException, TeamDoesntExistException {
-		int maximumCardInRound = 0;
-		Player roundWinner = null;
-
-		for (Slot actualSlot : slots) {
-			if (actualSlot.getLastOne().getValue() > maximumCardInRound) {
-				maximumCardInRound = actualSlot.getLastOne().getValue();
-				roundWinner =  actualSlot.getPlayer();
-			}
-		}
-        this.roundWinsPerTeam[roundWinner.getIdNumber()-1]++;
-        return roundWinner;
-	}
-
-    public void setWinnerOfGame(LinkedList<Slot> slots, Games actualGame) throws NotCardThrownException, TeamDoesntExistException {
-        if (roundWinsPerTeam[0] > roundWinsPerTeam[1]) {
-            scoreboard.increaseTheScoreOf(slots.get(0).getPlayer() , actualGame);
-        }
-        else
-        {
-            scoreboard.increaseTheScoreOf(slots.get(1).getPlayer() , actualGame);
-        }
 
     }
 
+    public Player setWinnerOfTheRound(LinkedList<Slot> slots) throws NotCardThrownException, TeamDoesntExistException {
+        int maximumCardInRound = 0;
+        Player roundWinner = null;
 
+        for (Slot actualSlot : slots) {
+            if (actualSlot.getLastOne().getValue() > maximumCardInRound) {
+                maximumCardInRound = actualSlot.getLastOne().getValue();
+                roundWinner = actualSlot.getPlayer();
+            }
+        }
+        this.roundWinsPerTeam[roundWinner.getIdNumber() - 1]++;
+        return roundWinner;
+    }
+
+    public void setWinnerOfGame(LinkedList<Slot> slots, Games actualGame) throws NotCardThrownException, TeamDoesntExistException {
+        if (roundWinsPerTeam[0] > roundWinsPerTeam[1]) {
+            scoreboard.increaseTheScoreOf( slots.get( 0 ).getPlayer(), actualGame );
+        } else {
+            scoreboard.increaseTheScoreOf( slots.get( 1 ).getPlayer(), actualGame );
+        }
+
+    }
 
 
 }
