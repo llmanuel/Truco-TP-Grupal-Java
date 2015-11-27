672fdec2c758ba2dfe92a7f20e8225064363360a
Matias Rozanec <rozanecm@gmail.com>
2015-11-27 12:46:07 -0300

    Completo metodo de creacion de jugadores, considerando los tres casos posibles (2, 4, 6 jugadores)

--- src/model/Builder.java
+++ src/model/Builder.java
@@ -16,13 +16,41 @@ public class Builder {
 
     private void createPlayers(int numberOfPlayers) {
         switch (numberOfPlayers){
-//            case 2: createTwoPlayers();
-//                break;
-//            case 4: createFourPlayers();
-//                break;
-//            case 6: createSixPlayers();
-//                break;
+            case 2: createTwoPlayers();
+                break;
+            case 4: createFourPlayers();
+                break;
+            case 6: createSixPlayers();
+                break;
         }
+    }
+
+    private void createTwoPlayers() {
+        Player player1 = new HumanPlayer(1);
+        Player player2 = new HumanPlayer(2);
+
+        players.add(player1);
+        players.add(player2);
+    }
+
+    private void createFourPlayers() {
+        createTwoPlayers();
+
+        Player player3 = new HumanPlayer(3);
+        Player player4 = new HumanPlayer(4);
+
+        players.add(player3);
+        players.add(player4);
+    }
+
+    private void createSixPlayers() {
+        createFourPlayers();
+
+        Player player5 = new HumanPlayer(5);
+        Player player6 = new HumanPlayer(6);
+
+        players.add(player5);
+        players.add(player6);
     }
 
     private void validateNumberOfPlayers(int numberOfPlayers) throws InvalidNumberOfPlayersException {
