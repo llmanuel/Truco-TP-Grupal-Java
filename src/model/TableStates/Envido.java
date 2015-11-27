ec4850f54ff79889166b238b034166804123720b
llmanuco <llauromanuel@gmail.com>
2015-11-27 14:38:53 -0300

    Trabajo sobre los cantos de los jugadores

--- src/model/TableStates/Envido.java
+++ src/model/TableStates/Envido.java
@@ -43,7 +43,7 @@ public class Envido implements Games {
 
     @Override
     public void iDontWannaAnswer() {
-
+        this.theTable.askNextPlayerOnTheTeam(theTable.getTeamCursor());
     }
 
     @Override
