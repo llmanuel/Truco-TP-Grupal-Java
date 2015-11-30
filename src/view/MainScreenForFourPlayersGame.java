8f560e3c1fe5acdbc3099568587da4e82bba5621
Alexis Daciuk <adaciuk@gmail.com>
2015-11-30 15:48:14 -0300

    Agrego vista para cuatro y seis jugadores

---
+++ src/view/MainScreenForFourPlayersGame.java
@@ -0,0 +1,4 @@
+package view;
+
+public class MainScreenForFourPlayersGame extends javax.swing.JFrame {
+}
---
+++ src/view/MainScreenForSixPlayersGame.java
@@ -0,0 +1,4 @@
+package view;
+
+public class MainScreenForSixPlayersGame extends javax.swing.JFrame {
+}
