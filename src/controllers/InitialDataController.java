7f4774411543db692197f33ea1b5280c2761a730
Alexis Daciuk <adaciuk@gmail.com>
2015-11-30 16:55:37 -0300

    Quito metodo inecesario

--- src/controllers/InitialDataController.java
+++ src/controllers/InitialDataController.java
@@ -1,7 +1,19 @@
 package controllers;
 
+import model.Builder;
+import model.Exceptions.InvalidNumberOfPlayersException;
+
 public class InitialDataController {
 
+    private Builder builder;
+
     public void prepareTableFor(int i) {
+        try {
+            builder = new Builder( i );
+        } catch (InvalidNumberOfPlayersException e) {
+
+        }
+
     }
+
 }
