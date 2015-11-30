43ead61f4b1aeda827b81ed52704a72514869fa0
Alexis Daciuk <adaciuk@gmail.com>
2015-11-30 18:20:14 -0300

    Alto refactor, quito cosas alpedo y cambio nombre de variables por nombres mas descriptivos

--- src/view/ScreenToAskForInitialData.java
+++ src/view/ScreenToAskForInitialData.java
@@ -3,33 +3,17 @@ package view;
 import controllers.InitialDataController;
 import controllers.TwoPlayersMatchController;
 import model.Team;
-
 import java.awt.*;
 import java.util.LinkedList;
 
-/**
- * Created by lucas on 27/11/15.
- */
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 public class ScreenToAskForInitialData extends javax.swing.JFrame {
 
     private static final String textFor2PlayerOption = "2 Jugadores";
     private static final String textFor4PlayerOption = "4 Jugadores";
     private static final String textFor6PlayerOption = "6 Jugadores";
     private InitialDataController controller;
+    int numberOfPlayers;
 
-    public Integer getNumberOfPlayers() {
-        return numberOfPlayers;
-    }
-
-    Integer numberOfPlayers;
-    /**
-     * Creates new form TtestUIEditor
-     */
     public ScreenToAskForInitialData() {
         initComponents();
     }
@@ -43,36 +27,36 @@ public class ScreenToAskForInitialData extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
-        listaDesplegableDeCantidadDeJugadores = new javax.swing.JComboBox<>();
-        separador = new javax.swing.JSeparator();
+        welcomeMessage = new javax.swing.JLabel();
+        askForPlayersQuantityMessage = new javax.swing.JLabel();
+        startGameButton = new javax.swing.JButton();
+        playerQuantityList = new javax.swing.JComboBox<>();
+        separator = new javax.swing.JSeparator();
         controller = new InitialDataController();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Bienvenido A Truco");
-        setName("PantallaBienvenida"); // NOI18N
+        setName("PantallaBienvenida");
         setPreferredSize(new java.awt.Dimension(600, 250));
         setResizable(false);
 
-        jLabel1.setFont(new java.awt.Font("Arial", Font.BOLD, 36)); // NOI18N
-        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel1.setText("Bienvenido a Truco");
-        jLabel1.setAlignmentY(0.0F);
+        welcomeMessage.setFont(new java.awt.Font("Arial", Font.BOLD, 36)); // NOI18N
+        welcomeMessage.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        welcomeMessage.setText("Bienvenido a Truco");
+        welcomeMessage.setAlignmentY(0.0F);
 
-        jLabel2.setFont(new java.awt.Font("Arial", Font.PLAIN, 14)); // NOI18N
-        jLabel2.setText("Por favor, seleccione la cantidad de jugadores");
+        askForPlayersQuantityMessage.setFont(new java.awt.Font("Arial", Font.PLAIN, 14)); // NOI18N
+        askForPlayersQuantityMessage.setText("Por favor, seleccione la cantidad de jugadores");
 
-        jButton1.setText("Comenzar Partida");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        startGameButton.setText("Comenzar Partida");
+        startGameButton.addActionListener( new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton1ActionPerformed(evt);
             }
         });
 
-        listaDesplegableDeCantidadDeJugadores.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "2 Jugadores", "4 Jugadores", "6 Jugadores" }));
-        listaDesplegableDeCantidadDeJugadores.setBorder(null);
+        playerQuantityList.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "2 Jugadores", "4 Jugadores", "6 Jugadores" }));
+        playerQuantityList.setBorder(null);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -80,36 +64,36 @@ public class ScreenToAskForInitialData extends javax.swing.JFrame {
                 layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                         .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                                 .addGap(0, 0, Short.MAX_VALUE)
-                                .addComponent(jButton1)
+                                .addComponent( startGameButton )
                                 .addGap(183, 183, 183))
                         .addGroup(layout.createSequentialGroup()
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                         .addGroup(layout.createSequentialGroup()
                                                 .addGap(92, 92, 92)
-                                                .addComponent(jLabel1))
+                                                .addComponent( welcomeMessage ))
                                         .addGroup(layout.createSequentialGroup()
                                                 .addGap(78, 78, 78)
-                                                .addComponent(listaDesplegableDeCantidadDeJugadores, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                                .addComponent( playerQuantityList, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                                         .addGroup(layout.createSequentialGroup()
                                                 .addGap(33, 33, 33)
                                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                                        .addComponent(jLabel2)
-                                                        .addComponent(separador, javax.swing.GroupLayout.PREFERRED_SIZE, 439, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                                                        .addComponent( askForPlayersQuantityMessage )
+                                                        .addComponent( separator, javax.swing.GroupLayout.PREFERRED_SIZE, 439, javax.swing.GroupLayout.PREFERRED_SIZE))))
                                 .addContainerGap(55, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
                 layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                         .addGroup(layout.createSequentialGroup()
                                 .addContainerGap()
-                                .addComponent(jLabel1)
+                                .addComponent( welcomeMessage )
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(separador, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent( separator, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addGap(13, 13, 13)
-                                .addComponent(jLabel2)
+                                .addComponent( askForPlayersQuantityMessage )
                                 .addGap(18, 18, 18)
-                                .addComponent(listaDesplegableDeCantidadDeJugadores, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent( playerQuantityList, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addGap(18, 18, 18)
-                                .addComponent(jButton1)
+                                .addComponent( startGameButton )
                                 .addContainerGap(150, Short.MAX_VALUE))
         );
 
@@ -118,9 +102,9 @@ public class ScreenToAskForInitialData extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
-        String opcionSeleccionada = (String) listaDesplegableDeCantidadDeJugadores.getSelectedItem();
+        String option = (String) playerQuantityList.getSelectedItem();
         LinkedList<Team> teams = new LinkedList<Team>(  );
-        switch (opcionSeleccionada){
+        switch (option){
             case textFor2PlayerOption:
 
                 numberOfPlayers = 2;
@@ -192,10 +176,10 @@ public class ScreenToAskForInitialData extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JComboBox<String> listaDesplegableDeCantidadDeJugadores;
-    private javax.swing.JSeparator separador;
+    private javax.swing.JButton startGameButton;
+    private javax.swing.JLabel welcomeMessage;
+    private javax.swing.JLabel askForPlayersQuantityMessage;
+    private javax.swing.JComboBox<String> playerQuantityList;
+    private javax.swing.JSeparator separator;
     // End of variables declaration//GEN-END:variables
 }
