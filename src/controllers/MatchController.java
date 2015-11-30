da34c11ba2614591bfe35d28a9e8d69c84eb1a7a
Lucas Risaro <lucasrisaro@gmail.com>
2015-11-30 15:37:48 -0300

    Comienzo de los llamados de las juegos

--- src/controllers/MatchController.java
+++ src/controllers/MatchController.java
@@ -1,11 +1,31 @@
 package controllers;
 
+import model.Exceptions.InvalidGameCallException;
+import model.Exceptions.NotCardThrownException;
+import model.Exceptions.NotYourTurnException;
+import model.Exceptions.TeamDoesntExistException;
+import model.Table;
+
 /**
  * Created by lucas on 30/11/15.
  */
+
 public class MatchController {
 
+    private Table table;
 
+    private void setTable(Table newTable) {
+
+        table = newTable;
+    }
+
+    public void callEnvido() throws InvalidGameCallException, NotYourTurnException, NotCardThrownException, TeamDoesntExistException {
+
+        table.getActualPlayer().callEnvido();
+
+   }
+
+    
 
 
 
