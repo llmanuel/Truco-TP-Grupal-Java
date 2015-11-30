819ff71f7c577b9f33deb5514b0a03d4fa3d817e
Matias Rozanec <rozanecm@gmail.com>
2015-11-30 15:03:18 -0300

    Elimino parte comentada obsoleta

--- src/view/ScreenToAskForInitialData.java
+++ src/view/ScreenToAskForInitialData.java
@@ -4,28 +4,6 @@ package view;
  * Created by lucas on 27/11/15.
  */
 /*
-import javax.swing.*;
-import java.awt.event.KeyEvent;
-
-public class ScreenToAskForInitialData extends JFrame {
-
-
-    public static void main(String[] args) {
-
-        JFrame frame = new JFrame("Configuracion inicial.");
-
-        frame.setVisible(true);
-        frame.setSize(500, 600);
-
-        JCheckBox numberOfPlayers = new JCheckBox("Chin");
-        numberOfPlayers.setMnemonic(KeyEvent.VK_C);
-        numberOfPlayers.setSelected(true);
-
-       // numberOfPlayers.addItemListener(this);
-    }
-}*/
-
-/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
