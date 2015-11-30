b75661ea355555da89b984ee97ef0dcf457f3279
Alexis Daciuk <adaciuk@gmail.com>
2015-11-30 15:10:58 -0300

    Cambio en nombre de la clase

--- src/view/MainScreenForTwoPlayersGame.java
+++ src/view/MainScreenForTwoPlayersGame.java
@@ -1,12 +1,12 @@
 package view;
 
-public class PrincipalScreenForTwoPlayersGame extends javax.swing.JFrame {
+public class MainScreenForTwoPlayersGame extends javax.swing.JFrame {
 
 
     /**
      * Creates new form VentanaPrincipalDeJuego
      */
-    public PrincipalScreenForTwoPlayersGame() {
+    public MainScreenForTwoPlayersGame() {
         initComponents();
     }
 
@@ -269,20 +269,20 @@ public class PrincipalScreenForTwoPlayersGame extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(PrincipalScreenForTwoPlayersGame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger( MainScreenForTwoPlayersGame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(PrincipalScreenForTwoPlayersGame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger( MainScreenForTwoPlayersGame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(PrincipalScreenForTwoPlayersGame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger( MainScreenForTwoPlayersGame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(PrincipalScreenForTwoPlayersGame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger( MainScreenForTwoPlayersGame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new PrincipalScreenForTwoPlayersGame().setVisible(true);
+                new MainScreenForTwoPlayersGame().setVisible(true);
             }
         });
 
