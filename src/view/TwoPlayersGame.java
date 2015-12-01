3edbb4b05641fffa58719f60bd14e150277c1850
Alexis Daciuk <adaciuk@gmail.com>
2015-11-30 22:44:10 -0300

    Actualizo vista con los EventHandler de los botones implementados

--- src/view/TwoPlayersGame.java
+++ src/view/TwoPlayersGame.java
@@ -1,5 +1,11 @@
 package view;
 
+import controllers.TwoPlayersMatchController;
+import model.Exceptions.InvalidGameCallException;
+import model.Exceptions.NotCardThrownException;
+import model.Exceptions.NotYourTurnException;
+import model.Exceptions.TeamDoesntExistException;
+
 import javax.swing.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
@@ -27,6 +33,8 @@ public class TwoPlayersGame extends JFrame {
     private JLabel card2SlotPlayerInTurn;
     private JLabel card3SlotPlayerInTurn;
     private JLabel playerInTurnName;
+    private JButton meVoyAlMazoButton;
+    private TwoPlayersMatchController controller;
 
     public TwoPlayersGame() {
         super("Truco");
@@ -43,58 +51,159 @@ public class TwoPlayersGame extends JFrame {
         envidoButton.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent mouseEvent) {
-                super.mouseClicked(mouseEvent);
+                envidoButtonClicked(mouseEvent);
             }
         });
         realEnvidoButton.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent mouseEvent) {
-                super.mouseClicked(mouseEvent);
+                realEnvidoButtonClicked(mouseEvent);
             }
         });
         faltaEnvidoButton.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent mouseEvent) {
-                super.mouseClicked(mouseEvent);
+                faltaEnvidoButtonClicked(mouseEvent);
             }
         });
         trucoButton.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent mouseEvent) {
-                super.mouseClicked(mouseEvent);
+                trucoButtonClicked(mouseEvent);
             }
         });
         retrucoButton.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent mouseEvent) {
-                super.mouseClicked(mouseEvent);
+                retrucoButtonClicked(mouseEvent);
             }
         });
         vale4Button.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent mouseEvent) {
-                super.mouseClicked(mouseEvent);
+                vale4ButtonClicked(mouseEvent);
             }
+
         });
         florButton.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent mouseEvent) {
-                super.mouseClicked(mouseEvent);
+                florButtonClicked(mouseEvent);
             }
         });
         quieroButton.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent mouseEvent) {
-                super.mouseClicked(mouseEvent);
+                quieroButtonClicked(mouseEvent);
             }
         });
         noQuieroButton.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent mouseEvent) {
-                super.mouseClicked(mouseEvent);
+                noQuieroButtonClicked(mouseEvent);
             }
         });
 
+    }
+
+    private void noQuieroButtonClicked(MouseEvent mouseEvent) {
+        try {
+            this.controller.giveUpGame();
+        } catch (NotYourTurnException e) {
+            e.printStackTrace();
+        } catch (NotCardThrownException e) {
+            e.printStackTrace();
+        } catch (TeamDoesntExistException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void quieroButtonClicked(MouseEvent mouseEvent) {
+        try {
+            this.controller.acceptCall();
+        } catch (NotCardThrownException e) {
+            e.printStackTrace();
+        } catch (NotYourTurnException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void florButtonClicked(MouseEvent mouseEvent) {
+        this.controller.callFlor();
+    }
+
+    private void vale4ButtonClicked(MouseEvent mouseEvent) {
+        try {
+            this.controller.callVale4();
+        } catch (NotYourTurnException e) {
+            e.printStackTrace();
+        } catch (NotCardThrownException e) {
+            e.printStackTrace();
+        } catch (TeamDoesntExistException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void retrucoButtonClicked(MouseEvent mouseEvent) {
+        try {
+            this.controller.callReTruco();
+        } catch (NotYourTurnException e) {
+            e.printStackTrace();
+        } catch (NotCardThrownException e) {
+            e.printStackTrace();
+        } catch (TeamDoesntExistException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void trucoButtonClicked(MouseEvent mouseEvent) {
+        try {
+            this.controller.callTruco();
+        } catch (NotYourTurnException e) {
+            e.printStackTrace();
+        } catch (NotCardThrownException e) {
+            e.printStackTrace();
+        } catch (TeamDoesntExistException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void faltaEnvidoButtonClicked(MouseEvent mouseEvent) {
+        try {
+            this.controller.callFaltaEnvido();
+        } catch (NotYourTurnException e) {
+            e.printStackTrace();
+        } catch (NotCardThrownException e) {
+            e.printStackTrace();
+        } catch (TeamDoesntExistException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void realEnvidoButtonClicked(MouseEvent mouseEvent) {
+        try {
+            this.controller.callRealEnvido();
+        } catch (NotYourTurnException e) {
+            e.printStackTrace();
+        } catch (NotCardThrownException e) {
+            e.printStackTrace();
+        } catch (TeamDoesntExistException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void envidoButtonClicked(MouseEvent mouseEvent) {
+        try {
+            this.controller.callEnvido();
+        } catch (InvalidGameCallException e) {
+            e.printStackTrace();
+        } catch (NotYourTurnException e) {
+            e.printStackTrace();
+        } catch (NotCardThrownException e) {
+            e.printStackTrace();
+        } catch (TeamDoesntExistException e) {
+            e.printStackTrace();
+        }
     }
 
     public void drawCardsPlayerInTurn(){
@@ -105,4 +214,7 @@ public class TwoPlayersGame extends JFrame {
 
     }
 
+    public void setController(TwoPlayersMatchController controller) {
+        this.controller = controller;
+    }
 }
