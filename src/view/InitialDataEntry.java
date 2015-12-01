f7d9c2e6a9760f677236612923975e07c6adb9ad
Alexis Daciuk <adaciuk@gmail.com>
2015-12-01 12:26:12 -0300

    Actualizo nueva vista de Data entry

--- src/view/InitialDataEntry.java
+++ src/view/InitialDataEntry.java
@@ -1,6 +1,20 @@
 package view;
 
 import javax.swing.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class InitialDataEntry extends JFrame {
+    private JComboBox playerQuantityComboBox;
+    private JButton empezarJuegoButton;
+    private JLabel welcomeMessageLabel;
+
+    public InitialDataEntry() {
+        empezarJuegoButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent mouseEvent) {
+                super.mouseClicked(mouseEvent);
+            }
+        });
+    }
 }
