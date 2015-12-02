8979b0f57e2f58065ce184f0d7aaee2e73f6bad4
Alexis Daciuk <adaciuk@gmail.com>
2015-12-01 22:15:19 -0300

    Implemento mensajes a la Table

--- src/controllers/FourPlayersController.java
+++ src/controllers/FourPlayersController.java
@@ -1,4 +1,54 @@
 package controllers;
 
+import model.Exceptions.NotCardThrownException;
+import model.Exceptions.NotYourTurnException;
+import model.Exceptions.TeamDoesntExistException;
+import model.Table;
+import view.FourPlayersGame;
+
 public class FourPlayersController {
+    private final Table table;
+    private final FourPlayersGame gameView;
+
+    public FourPlayersController(FourPlayersGame fourPlayersGameView, Table table) {
+        this.table = table;
+        this.gameView = fourPlayersGameView;
+
+    }
+
+    public void giveUpGame() throws NotYourTurnException, NotCardThrownException, TeamDoesntExistException {
+        table.getActualPlayer().giveUp();
+    }
+
+    public void acceptCall() throws NotCardThrownException, NotYourTurnException {
+        table.getActualPlayer().acceptCall();
+    }
+
+    public void callFlor() {
+        table.getActualPlayer().callFlor();
+    }
+
+    public void callVale4() throws NotYourTurnException, NotCardThrownException, TeamDoesntExistException {
+        table.getActualPlayer().callVale4();
+    }
+
+    public void callReTruco() throws NotYourTurnException, NotCardThrownException, TeamDoesntExistException {
+        table.getActualPlayer().callReTruco();
+    }
+
+    public void callTruco() throws NotYourTurnException, NotCardThrownException, TeamDoesntExistException {
+        table.getActualPlayer().callTruco();
+    }
+
+    public void callFaltaEnvido() throws NotYourTurnException, NotCardThrownException, TeamDoesntExistException {
+        table.getActualPlayer().callFaltaEnvido();
+    }
+
+    public void callRealEnvido() throws NotYourTurnException, NotCardThrownException, TeamDoesntExistException {
+        table.getActualPlayer().callRealEnvido();
+    }
+
+    public void callEnvido() throws NotYourTurnException, NotCardThrownException, TeamDoesntExistException {
+        table.getActualPlayer().callEnvido();
+    }
 }
