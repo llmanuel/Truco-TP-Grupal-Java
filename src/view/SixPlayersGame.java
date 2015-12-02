9b31a8b1b22bac8444468bb8cce34c2922920551
Alexis Daciuk <adaciuk@gmail.com>
2015-12-01 23:04:15 -0300

    Empiezo a dar forma a la vista de seis jugadores

--- src/view/SixPlayersGame.java
+++ src/view/SixPlayersGame.java
@@ -3,4 +3,14 @@ package view;
 import javax.swing.*;
 
 public class SixPlayersGame extends JFrame{
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
+    private JButton button6;
+    private JButton button7;
+    private JButton button8;
+    private JButton button9;
+    private JButton button10;
 }
