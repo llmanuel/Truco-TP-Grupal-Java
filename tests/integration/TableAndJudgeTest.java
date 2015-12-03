7e1e8f74c352567a6fde4740baf38b35eecbf9a1
Alexis Daciuk <adaciuk@gmail.com>
2015-12-03 17:29:02 -0300

    Agrego test

--- tests/integration/TableAndJudgeTest.java
+++ tests/integration/TableAndJudgeTest.java
@@ -15,59 +15,79 @@ public class TableAndJudgeTest {
     private Table table;
     private Judge judge;
     private Scoreboard scoreboard;
-    private LinkedList<Player> fourPlayers;
-    private LinkedList<Team> fourPlayersTeams;
+    private LinkedList<Player> twoPlayers;
+    private LinkedList<Team> twoPlayersTeam;
     private Team teamMyPlayer;
     private Team teamOtherPlayer;
-    private Team teamCharles;
-    private Team teamNick;
+    HumanPlayer myPlayer;
+    HumanPlayer otherPLayer;
+    private Card card1;
+    private Card card2;
+    private Card card3;
+    private Card card4;
+    private Card card5;
+    private Card card6;
+    private LinkedList<Card> hand1Cards;
+    private LinkedList<Card> hand2Cards;
+    private Hand hand1;
+    private Hand hand2;
 
     @Before
     public void setup() throws InvalidNumberOfPlayersException {
-        HumanPlayer myPlayer = new HumanPlayer(1);
+        card1 = new Card( 5, "BASTO", 2 );
+        card2 = new Card( 7, "BASTO", 4 );
+        card3 = new Card( 7, "ESPADA", 12 );
+
+        card4 = new Card( 12, "ESPADA", 7 );
+        card5 = new Card( 3, "ORO", 10 );
+        card6 = new Card( 7, "ORO", 11 );
+
+        hand1Cards = new LinkedList<Card>();
+        hand1Cards.add( card1 );
+        hand1Cards.add( card2 );
+        hand1Cards.add( card3 );
+
+        hand2Cards = new LinkedList<Card>();
+        hand2Cards.add( card4 );
+        hand2Cards.add( card5 );
+        hand2Cards.add( card6 );
+
+        hand1 = new Hand( hand1Cards );
+        hand2 = new Hand( hand2Cards );
+
+
+        myPlayer = new HumanPlayer(1);
         LinkedList<Player> myPlayerList= new LinkedList<Player>(  );
         myPlayerList.add( myPlayer );
 
-        HumanPlayer otherPLayer = new HumanPlayer(2);
+        otherPLayer = new HumanPlayer(2);
         LinkedList<Player> otherPlayerList = new LinkedList<Player>(  );
         otherPlayerList.add( otherPLayer );
 
-        HumanPlayer charles = new HumanPlayer(3);
-        LinkedList<Player> charlesList= new LinkedList<Player>(  );
-        charlesList.add( charles );
-
-        HumanPlayer nick = new HumanPlayer(4);
-        LinkedList<Player> nickList = new LinkedList<Player>(  );
-        nickList.add( nick );
-
-
         teamMyPlayer = new Team( myPlayerList );
         teamOtherPlayer = new Team( otherPlayerList );
-        teamCharles = new Team( charlesList );
-        teamNick = new Team( nickList );
 
+        twoPlayers = new LinkedList<Player>();
+        twoPlayers.addLast(myPlayer);
+        twoPlayers.addLast(otherPLayer);
 
-        fourPlayers = new LinkedList<Player>();
-        fourPlayers.addLast(myPlayer);
-        fourPlayers.addLast(otherPLayer);
-        fourPlayers.addLast(charles);
-        fourPlayers.addLast(nick);
-
-        fourPlayersTeams = new LinkedList<Team>();
-        fourPlayersTeams.addLast(teamMyPlayer);
-        fourPlayersTeams.addLast(teamOtherPlayer);
-        fourPlayersTeams.addLast(teamCharles);
-        fourPlayersTeams.addLast(teamNick);
+        twoPlayersTeam = new LinkedList<Team>();
+        twoPlayersTeam.addLast(teamMyPlayer);
+        twoPlayersTeam.addLast(teamOtherPlayer);
 
         this.table = new Table();
 
+        myPlayer.setTable( this.table );
+        otherPLayer.setTable( this.table );
 
-        this.scoreboard = new Scoreboard( fourPlayersTeams, this.table );
+        this.scoreboard = new Scoreboard( twoPlayersTeam, this.table );
 
-        this.table.letSitThese( fourPlayers );
+        this.table.letSitThese( twoPlayers );
         this.table.setGame();
 
         this.judge = new Judge( this.scoreboard );
+
+        this.table.setJudge( this.judge );
 
     }
 
@@ -77,6 +97,16 @@ public class TableAndJudgeTest {
         this.judge.setWinnerOfTheRound( this.table.getSlots());
     }
 
+    @Test
+    public void judgeReturnWinnerOfARoundCorrectly() throws NotYourTurnException, DonTHaveThatCardException, NotCardThrownException, TeamDoesntExistException {
+        myPlayer.setHand( hand1 );
+        otherPLayer.setHand( hand2 );
+
+        myPlayer.playCard( card1 );
+        otherPLayer.playCard( card4 );
+
+
+    }
 
 
 
