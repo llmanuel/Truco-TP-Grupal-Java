141522738b0f4e454b4eda6c9fdecc948f77cbba
Lucas Risaro <lucasrisaro@gmail.com>
2015-12-08 13:47:41 -0300

    Comento el test para chequear el funcionamiento del metodo para obtener el valor maximo de las cartas jugadas en un round.

--- tests/unit/CpuPlayerTest.java
+++ tests/unit/CpuPlayerTest.java
@@ -1,6 +1,5 @@
-import model.Card;
-import model.CpuPlayer;
-import model.Hand;
+import model.*;
+import model.Exceptions.*;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -9,9 +8,9 @@ import java.util.LinkedList;
 
 public class CpuPlayerTest {
 
-    private Card card1, card2, card3;
-    private LinkedList<Card> hand1Cards;
-    private Hand hand1;
+    private Card card1, card2, card3, card4, card5, card6;
+    private LinkedList<Card> hand1Cards, hand2Cards;
+    private Hand hand1, hand2;
 
     @Before
     public void setup() {
@@ -19,13 +18,23 @@ public class CpuPlayerTest {
         card2 = new Card( 7, "BASTO", 4 );
         card3 = new Card( 7, "ESPADA", 12 );
 
+        card4 = new Card(5, "ORO", 2);
+        card5 = new Card(7, "ORO", 11);
+        card6 = new Card(7, "ESPADA", 12);
+
+
         hand1Cards = new LinkedList<Card>();
         hand1Cards.add( card1 );
         hand1Cards.add( card2 );
         hand1Cards.add( card3 );
 
-        hand1 = new Hand( hand1Cards );
+        hand2Cards = new LinkedList<Card>();
+        hand2Cards.add( card4 );
+        hand2Cards.add( card5 );
+        hand2Cards.add( card6 );
 
+        hand1 = new Hand( hand1Cards );
+        hand2 = new Hand(hand2Cards);
     }
 
     @Test
@@ -98,5 +107,30 @@ public class CpuPlayerTest {
         Assert.assertTrue(cards.get(0).getValue() == 2);
         Assert.assertTrue(cards.get(1).getValue() == 4);
         Assert.assertTrue(cards.get(2).getValue() == 12 );
+    }
+
+    @Test
+    public void canSearchTheHigherCardPlayedInTheRound() throws InvalidNumberOfPlayersException, NotYourTurnException, DonTHaveThatCardException, NotCardThrownException, TeamDoesntExistException {
+        HumanPlayer firstPlayer = new HumanPlayer(1);
+        CpuPlayer secondPlayer = new CpuPlayer(2);
+
+        firstPlayer.setHand(hand1);
+        secondPlayer.setHand(hand2);
+
+        LinkedList<Player> TwoPlayers = new LinkedList<Player>();
+        TwoPlayers.addLast(firstPlayer);
+        TwoPlayers.addLast(secondPlayer);
+
+        Table table = new Table();
+        table.letSitThese(TwoPlayers);
+        table.setGame();
+        table.setRoundBeginner(firstPlayer);
+
+        firstPlayer.playCard(card3);
+
+        secondPlayer.setTable(table);
+
+        Assert.assertTrue(secondPlayer.searchHigherCardInTheRound() == 12);
+
     }
 }
