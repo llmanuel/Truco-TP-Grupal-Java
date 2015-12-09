3e78f715248104498ac2819f83dd56a0bfe3cb78
Alexis Daciuk <adaciuk@gmail.com>
2015-12-09 17:15:16 -0300

    Voy completando el setup

--- tests/integration/GameWithSixPlayersTest.java
+++ tests/integration/GameWithSixPlayersTest.java
@@ -21,6 +21,19 @@ public class GameWithSixPlayersTest {
     private Builder builder;
     private Table table;
     private LinkedList<Player> players;
+    private Card card7;
+    private Card card8;
+    private Card card9;
+    private Card card10;
+    private Card card11;
+    private Card card12;
+    private Card card13;
+    private Card card14;
+    private Card card15;
+    private Card card16;
+    private Card card17;
+    private Card card18;
+    private LinkedList<Card> hand3Cards;
 
     @Before
     public void setup() throws InvalidNumberOfPlayersException {
@@ -32,6 +45,23 @@ public class GameWithSixPlayersTest {
         card5 = new Card(3, "ORO", 10);
         card6 = new Card(7, "ORO", 11);
 
+        card7 = new Card(4, "ORO", 1);
+        card8 = new Card(5, "ORO", 2);
+        card9 = new Card(2, "ORO", 9);
+
+        card10 = new Card(11, "ESPADA", 6);
+        card11 = new Card(6, "ORO", 3);
+        card12 = new Card(1, "ORO", 8);
+
+        card13 = new Card(10, "ESPADA", 5);
+        card14 = new Card(1, "COPA", 8);
+        card15 = new Card(7, "COPA", 4);
+
+        card16 = new Card(12, "COPA", 7);
+        card17 = new Card(3, "COPA", 10);
+        card18 = new Card(6, "ESPADA", 11);
+
+
         hand1Cards = new LinkedList<Card>();
         hand1Cards.add(card1);
         hand1Cards.add(card2);
@@ -41,6 +71,12 @@ public class GameWithSixPlayersTest {
         hand2Cards.add(card4);
         hand2Cards.add(card5);
         hand2Cards.add(card6);
+
+        hand3Cards = new LinkedList<Card>();
+        hand3Cards.add(card7);
+        hand3Cards.add(card8);
+        hand3Cards.add(card9);
+
 
         hand1 = new Hand(hand1Cards);
         hand2 = new Hand(hand2Cards);
