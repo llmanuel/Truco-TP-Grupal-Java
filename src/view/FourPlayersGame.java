4be41c0e6c080be693122f01cb94e1f49c164585
Lucas Risaro <lucasrisaro@gmail.com>
2015-12-09 20:38:19 -0300

    Cambio de parametros del cartel para proximo jugador

--- src/view/FourPlayersGame.java
+++ src/view/FourPlayersGame.java
@@ -49,7 +49,7 @@ public class FourPlayersGame extends JFrame {
     private JLabel firstPlayerID;
     private JLabel secondPlayerID;
     private FourPlayersController controller;
-    private JDialog nextPlayer;
+
 
     public FourPlayersGame() throws NotCardThrownException {
         super("Truco");
@@ -159,7 +159,7 @@ public class FourPlayersGame extends JFrame {
 
     public void showMassageNextPlayer() {
 
-        JOptionPane.showMessageDialog(nextPlayer,"Turno del proximo jugador.","",JOptionPane.WARNING_MESSAGE);
+        JOptionPane.showMessageDialog(null,"Turno del proximo jugador.","",JOptionPane.WARNING_MESSAGE);
     }
 
     private BufferedImage getCardImage(Card actualCard){
