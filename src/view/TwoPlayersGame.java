891d8cef9e23e669af63864693d0cec0c69f7780
Alexis Daciuk <adaciuk@gmail.com>
2015-12-09 10:58:00 -0300

    Actualizo vista de 2 jugadores

--- src/view/TwoPlayersGame.java
+++ src/view/TwoPlayersGame.java
@@ -145,7 +145,7 @@ public class TwoPlayersGame extends JFrame {
     }
 
     private BufferedImage getCardImage(Card actualCard){
-        String imagePath = "resources/images/cards/" + actualCard.getSuit() + "/" + actualCard.getNumber() + ".png";
+        String imagePath = "resources/images/cards/105x160/" + actualCard.getSuit() + "/" + actualCard.getNumber() + ".png";
         BufferedImage cardPicture = null;
 
         try {
