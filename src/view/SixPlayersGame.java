1ed2b43c7b274f26d4b3d72ef93df323e2f4d9af
Alexis Daciuk <adaciuk@gmail.com>
2015-12-09 15:03:37 -0300

    Actualizo la vista, con los listeners de los botones

--- src/view/SixPlayersGame.java
+++ src/view/SixPlayersGame.java
@@ -1,8 +1,18 @@
 package view;
 
+import controllers.FourPlayersController;
+import controllers.SixPlayersController;
+import model.Exceptions.InvalidNumberOfPlayersException;
+import model.Exceptions.NotCardThrownException;
+import model.Exceptions.NotYourTurnException;
+import model.Exceptions.TeamDoesntExistException;
+
 import javax.swing.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class SixPlayersGame extends JFrame{
+    private final SixPlayersController controller;
     private JButton florButton;
     private JButton noQuieroButton;
     private JButton reTrucoButton;
@@ -20,4 +30,190 @@ public class SixPlayersGame extends JFrame{
     private JLabel team1ID;
     private JLabel team2Score;
     private JLabel team1Score;
+    private JLabel player1SameTeamID;
+    private JLabel card1SlotPlayer1SameTeam;
+    private JLabel card2SlotPlayer1SameTeam;
+    private JLabel card3SlotPlayer1SameTeam;
+    private JLabel player2OtherTeamID;
+    private JLabel card1SlotPlayer2OtherTeam;
+    private JLabel card2SlotPlayer2OtherTeam;
+    private JLabel card3SlotPlayer2OtherTeam;
+    private JLabel player2SameTeamID;
+    private JLabel card1SlotPlayer2SameTeam;
+    private JLabel card2SlotPlayer2SameTeam;
+    private JLabel card3SlotPlayer2SameTeam;
+    private JLabel player3OtherTeamID;
+    private JLabel card1SlotPlayer3OtherTeam;
+    private JLabel card2SlotPlayer3OtherTeam;
+    private JLabel card3SlotPlayer3OtherTeam;
+    private JLabel card1SlotPlayerInTurn;
+    private JLabel card2SlotPlayerInTurn;
+    private JLabel card3SlotPlayerInTurn;
+    private JLabel player1OtherTeamID;
+    private JLabel card3SlotPlayer1OtherTeam;
+    private JLabel card2SlotPlayer1OtherTeam;
+    private JLabel card1SlotPlayer1OtherTeam;
+    private JPanel rootPanel;
+}
+    public SixPlayersGame() throws NotCardThrownException {
+        super("Truco");
+
+        try {
+            controller = new SixPlayersController(this);
+        } catch (InvalidNumberOfPlayersException e) {
+            this.showMessage("Cantidad Invalida de Jugadores.");
+        }
+        setContentPane(rootPanel);
+
+        pack();
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+
+
+        envidoButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent mouseEvent) {
+                envidoButtonClicked();
+            }
+        });
+        realEnvidoButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent mouseEvent) {
+                realEnvidoButtonClicked();
+            }
+        });
+        faltaEnvidoButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent mouseEvent) {
+                faltaEnvidoButtonClicked();
+            }
+        });
+        trucoButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent mouseEvent) {
+                trucoButtonClicked();
+            }
+        });
+        reTrucoButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent mouseEvent) {
+                retrucoButtonClicked();
+            }
+        });
+        vale4Button.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent mouseEvent) {
+                vale4ButtonClicked();
+            }
+
+        });
+        florButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent mouseEvent) {
+                try {
+                    florButtonClicked();
+                } catch (NotYourTurnException e) {
+                    e.printStackTrace();
+                } catch (TeamDoesntExistException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        quieroButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent mouseEvent) {
+                quieroButtonClicked();
+            }
+        });
+        noQuieroButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent mouseEvent) {
+                noQuieroButtonClicked();
+            }
+        });
+        meVoyAlMazoButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent mouseEvent) {
+                meVoyAlMazoButtonClicked();
+            }
+        });
+
+
+        card1HandPlayerInTurn.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent mouseEvent) {
+                card1HandPlayerInTurnClicked();
+
+            }
+        });
+        card2HandPlayerInTurn.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent mouseEvent) {
+                card2HandPlayerInTurnClicked();
+
+            }
+        });
+        card3HandPlayerInTurn.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent mouseEvent) {
+                card3HandPlayerInTurnClicked();
+
+            }
+        });
+    }
+
+    private void card3HandPlayerInTurnClicked() {
+
+    }
+
+    private void card2HandPlayerInTurnClicked() {
+
+    }
+
+    private void card1HandPlayerInTurnClicked() {
+
+    }
+
+    private void meVoyAlMazoButtonClicked() {
+
+    }
+
+    private void noQuieroButtonClicked() {
+
+    }
+
+    private void quieroButtonClicked() {
+
+    }
+
+    private void florButtonClicked() {
+    }
+
+    private void vale4ButtonClicked() {
+
+    }
+
+    private void retrucoButtonClicked() {
+
+    }
+
+    private void trucoButtonClicked() {
+
+    }
+
+    private void faltaEnvidoButtonClicked() {
+
+    }
+
+    private void realEnvidoButtonClicked() {
+
+    }
+
+    private void envidoButtonClicked() {
+    }
+
+    private void showMessage(String message) {
+        JOptionPane.showMessageDialog( null, message );
+    }
 }
