fc307878399ebdad3a2949bf85cbaa60fca417e7
Lucas Risaro <lucasrisaro@gmail.com>
2015-12-09 15:10:50 -0300

    Comienzo del controlador para 6 jugadores.

--- src/controllers/SixPlayersController.java
+++ src/controllers/SixPlayersController.java
@@ -1,9 +1,32 @@
 package controllers;
 
+import model.Builder;
+import model.Exceptions.InvalidNumberOfPlayersException;
+import model.Player;
+import model.Table;
 import view.SixPlayersGame;
 
+import java.util.LinkedList;
+
 public class SixPlayersController {
-    public SixPlayersController(SixPlayersGame sixPlayersGame) {
+    private Table table;
+    private SixPlayersGame gameView;
+    private Builder builder;
+    private LinkedList<Player> players;
+    private Player cursor;
+
+    public SixPlayersController(SixPlayersGame sixPlayersGame) throws InvalidNumberOfPlayersException {
+
+        gameView = sixPlayersGame;
+        builder = new Builder(6);
+        players = builder.getPlayers();
+        this.table = builder.getTable();
+        this.startGame();
 
     }
+
+    private void startGame() {
+    }
+
+
 }
